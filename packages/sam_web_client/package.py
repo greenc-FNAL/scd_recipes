e504d73e8300e473135cc751701a046678ee1b05
mengel <mengel@fermicloudmwm02.fnal.gov>
2020-02-25 11:08:43 -0600

    samweb recipe draft

---
+++ packages/sam_web_client/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class SamWebClient (Package):
+    """Client package for SAM data management system webservice"""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/sam-web-client/wiki"
+    url      = "http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/sam-web-client.v3_0.tar"
+
+    version('2.1')
+    version('2.2')
+    version('3.0')
+    #version('3.1')
+
+    depends_on('python', type=('build','run'))
+    depends_on('py-requests',type='run')
+
+    def install(self, spec, prefix):
+        install_tree(self.stage.source_path, prefix.bin)
