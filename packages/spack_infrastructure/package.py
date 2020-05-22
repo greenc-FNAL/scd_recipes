2ca4d283bdabb817cd849c03a24aab2976d9ceb8
Marc Mengel <mengel@fnal.gov>
2020-05-22 16:05:17 -0500

    new version

--- packages/spack_infrastructure/package.py
+++ packages/spack_infrastructure/package.py
@@ -6,6 +6,7 @@ class SpackInfrastructure(Package):
     homepage = "http://cdcvs.fnal.gov/redmine/projects/spack-infrastrucutre"
     url = 'http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/spack-infrastructure.v1_2.tar'
 
+    version('1_4', sha256='fd405134ea1ae96d98aaae38c5849f213d47be96ba3043e195ce46e8799e5dae')
     version('1_3', sha256='8a00ee68d66f072987f164763545ce88e1f885e1475e2a77423133ae24e8ae16')
     version('1_2', sha256='8bab9965072f8ef4933e166fafb9a4c6cf14cbcdc8550b314a76d23bce926aa7')
 
