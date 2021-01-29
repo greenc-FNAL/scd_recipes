08ec2587a3fb2cb4c65f424e62fcb6ac24115743
Marc Mengel <mengel@fnal.gov>
2021-01-28 19:51:44 -0600

    building globus-toolkit now

--- packages/globus-toolkit/package.py
+++ packages/globus-toolkit/package.py
@@ -5,6 +5,7 @@
 
 
 from spack import *
+import os
 
 class GlobusToolkit(AutotoolsPackage):
     """FIXME: Put a proper description of your package here."""
@@ -19,5 +20,34 @@ class GlobusToolkit(AutotoolsPackage):
     version('5_2_4',    sha256='9fd4588c3a0ee6ff80bb8aa0390ab7cf39fa76709c2f61b2da3f2df273ac9c96')
     version('5_2_3',    sha256='a9b620c19f776f878b165f3a9027352fecefaa37cef90a95927e7901ff05e594')
 
+    patch('globus.patch')
+
+    depends_on('pkgconfig')
     depends_on('autoconf')
+    depends_on('automake')
+    depends_on('libtool')
     depends_on('openssl')
+
+    def autoreconf(self, spec, prefix):
+        # make sure there's an ltmain.sh
+        os.environ['HAVE_LTDL'] = '1'
+        open("ltmain.sh","a").close()
+        try:
+             autoreconf('-ivf')
+        except:
+             pass
+        try:
+            automake('--add-missing')
+        except:
+             pass
+        autoconf()
+        f = open("libltdl/Makefile", "w")
+        f.write("all:\n\ninstall:\n\n")
+        f.close()
+
+    def configure_args(self):
+        return  [
+              '--with-ltdl-include=%s' % self.spec['libtool'].prefix.include,
+              '--with-ltdl-lib=%s' % self.spec['libtool'].prefix.lib,
+            ]
+
