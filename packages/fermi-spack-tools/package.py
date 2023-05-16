0ef09e91a3d71a0cacfdd1af607bbc2c4fa55243
Marc Mengel <mengel@fnal.gov>
2023-05-16 15:58:34 -0500

    spack-infrastructure -> fermi-spack-tools

--- packages/fermi-spack-tools/package.py
+++ packages/fermi-spack-tools/package.py
@@ -1,15 +1,16 @@
 from spack import *
 
 
-class SpackInfrastructure(Package):
+class FermiSpackTools(Package):
     """Spack utility scripts, etc"""
 
-    homepage = "https://cdcvs.fnal.gov/redmine/projects/spack-infrastrucutre"
-    url = "https://github.com/FNALssi/spack-infrastructure/archive/refs/tags/v2.19.0.tar.gz"
+    homepage = "https://github.com/FNALssi/fermi-spack-tools"
+    url = "https://github.com/FNALssi/fermi-spack-tools/archive/refs/tags/v2.19.0.tar.gz"
+    git = "https://github.com/FNALssi/fermi-spack-tools"
 
     version("2.19.0", sha256="274aafb4ffad82205c8972c6aa85c04e5fdc86ce856c7701105b3b7a13159763")
     version("2_18_00_01", sha256="52d60684a359060ef95c91b9fb4adf0d114b28437144dc496118ccf39af89fd0")
-    version("master", git="https://github.com/FNALssi/spack-infrastructure/")
+    version("main", branch="main", git=git)
 
     def url_for_version(self, version):
         fmt="https://github.com/FNALssi/spack-infrastructure/archive/refs/tags/v{0}.tar.gz"
